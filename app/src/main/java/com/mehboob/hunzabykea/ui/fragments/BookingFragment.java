221b674643598a75ef6f1419a50ee5dee2e2ead1
Basit Murad <basitmurad7@gmail.com>
2023-03-23 23:55:06 +0500

    fourth commit

--- app/src/main/java/com/mehboob/hunzabykea/ui/fragments/BookingFragment.java
+++ app/src/main/java/com/mehboob/hunzabykea/ui/fragments/BookingFragment.java
@@ -10,13 +10,13 @@ import android.view.ViewGroup;
 
 import com.mehboob.hunzabykea.R;
 
-public class BokkingFragment extends Fragment {
+public class BookingFragment extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bokking, container, false);
+        return inflater.inflate(R.layout.fragment_booking, container, false);
     }
 }
