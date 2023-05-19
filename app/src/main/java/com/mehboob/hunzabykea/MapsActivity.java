f6ea06424a082d53a31a6fd41b09ced3341eea81
Mehboob-alam1 <ma8122576@gmail.com>
2023-05-19 15:44:30 +0000

    map sdk fixed

--- app/src/main/java/com/mehboob/hunzabykea/MapsActivity.java
+++ app/src/main/java/com/mehboob/hunzabykea/MapsActivity.java
@@ -1,126 +1,82 @@
 package com.mehboob.hunzabykea;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Intent;
+
 import android.content.pm.PackageManager;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
+
 import android.location.Location;
-import android.os.AsyncTask;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.location.Geocoder;
-import android.view.View;
-import android.widget.Toast;
 
-import com.akexorcist.googledirection.DirectionCallback;
-import com.akexorcist.googledirection.GoogleDirection;
-import com.akexorcist.googledirection.constant.AvoidType;
-import com.akexorcist.googledirection.constant.TransportMode;
-import com.akexorcist.googledirection.model.Direction;
-import com.directions.route.AbstractRouting;
-import com.directions.route.RouteException;
-import com.directions.route.Routing;
-import com.directions.route.RoutingListener;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.ButtCap;
-import com.google.android.gms.maps.model.JointType;
+
+
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.Place;
-import com.google.android.libraries.places.api.model.PlaceLikelihood;
-import com.google.android.libraries.places.api.model.RectangularBounds;
-import com.google.android.libraries.places.api.model.TypeFilter;
-import com.google.android.libraries.places.api.net.FetchPlaceRequest;
-import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
-import com.google.android.libraries.places.api.net.PlacesClient;
-import com.google.android.libraries.places.widget.Autocomplete;
-import com.google.android.libraries.places.widget.AutocompleteActivity;
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
-import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.maps.android.PolyUtil;
-import com.mapbox.maps.MapView;
-import com.mapbox.maps.MapboxMap;
-import com.mapbox.maps.Style;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.Style;
+
+import com.mapbox.mapboxsdk.style.layers.PropertyFactory;
+import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
+import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import com.mehboob.hunzabykea.databinding.ActivityMapsBinding;
-import com.mehboob.hunzabykea.ui.interfaces.ApiInterface;
-import com.mehboob.hunzabykea.ui.mapModels.Result;
-import com.mehboob.hunzabykea.ui.mapModels.Route;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-
-import io.reactivex.SingleObserver;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 
-public class MapsActivity extends AppCompatActivity  {
+
+public class MapsActivity extends AppCompatActivity implements PermissionsListener, OnMapReadyCallback {
 
 
     private ActivityMapsBinding binding;
-private MapView mapView;
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+    private static final String SOURCE_ID = "source-id";
+    private static final String LAYER_ID = "layer-id";
+    private PermissionsManager permissionsManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getResources().getString(R.string.mapbox_access_token));
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+
+
+        binding.mapView.getMapAsync(new com.mapbox.mapboxsdk.maps.OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull com.mapbox.mapboxsdk.maps.MapboxMap mapboxMap) {
+                MapsActivity.this.mapboxMap = mapboxMap;
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        mapboxMap.getUiSettings().setAttributionEnabled(false);
+                        enableLocationComponent(style);
+                    }
+                });
+            }
+        });
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
     }
+
 
     @SuppressLint("Lifecycle")
     @Override
@@ -148,5 +104,68 @@ binding.mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
     protected void onDestroy() {
         super.onDestroy();
         binding.mapView.onDestroy();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+
+    }
+
+    private void enableLocationComponent(Style style) {
+        // Check if location permissions are granted
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            mapboxMap.getLocationComponent().activateLocationComponent(this, mapboxMap.getStyle());
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            mapboxMap.getLocationComponent().setLocationComponentEnabled(true);
+
+            // Set the camera position to the user's current location
+            Location lastLocation = mapboxMap.getLocationComponent().getLastKnownLocation();
+            if (lastLocation != null) {
+                LatLng latLng = new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+                com.mapbox.mapboxsdk.geometry.LatLng latLng1= new com.mapbox.mapboxsdk.geometry.LatLng(lastLocation.getLatitude(),lastLocation.getLongitude());
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(latLng1)
+                        .zoom(14)
+                        .build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+//                mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom( (LatLng) new LatLng(lastLocation.getLatitude(),lastLocation.getLongitude()),12));
+            }
+
+            // Add a marker at the user's current location
+            style.addSource(new GeoJsonSource(SOURCE_ID));
+            style.addLayer(new SymbolLayer(LAYER_ID, SOURCE_ID)
+                    .withProperties(
+                            PropertyFactory.iconImage("your-icon-image"),
+                            PropertyFactory.iconSize(1.0f),
+                            PropertyFactory.iconOffset(new Float[] {0f, -16f})
+                    ));
+        } else {
+            // Request location permissions
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+    @Override
+    public void onPermissionResult(boolean granted) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
